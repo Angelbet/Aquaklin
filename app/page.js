3913859cd9e1d17bcbdb8835f28b57bce93fc271
Angelbet <angelfabian.becerra@gmail.com>
2023-07-14 15:18:16 -0400

    First Deploy, Page UnderConstruction

--- app/page.js
+++ app/page.js
@@ -1,95 +1,136 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+
+import style from '../styles/UnderConstruction.module.scss'
+import Link from 'next/link'
+
+// Images
+import logo from '../public/assets/Isologo.png'
+import effect_1 from '../public/assets/effect_1.png'
+
+// Fonts
+import { Raleway } from 'next/font/google'
+
+const raleway = Raleway({
+  weight: '600',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+// Gsap
+import { gsap } from 'gsap'
+import { Linear } from 'gsap'
 
 export default function Home() {
+
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+    <main className={style.body_content}>
+
+      {/* Logo */}
+      <a
+        href="/"
+        className={`${style.template_logo} ${style.w_inline_block}`}
+      >
+        <Image
+          src={logo}
+          width={300}
+          alt="Aquaklin Logo"
+          className={style.zork_logo}
+        />
+      </a>
+
+      <div className={style.hero}>
+        <div
+          id={style["w-node-_153fadda-452c-0f46-de80-5a387729fd05-e0473f29"]}
+          className={style.max_w_hero}
+        >
+          <h1 className={`${style.title_1} ${raleway.className}`}>
+            Aquaklin.
+            <br />
+            <span className={style.image_span}>
+              <br />
+            </span>
+            Próximamente.
+          </h1>
+        </div>
+        <div
+          id={style["w-node-_30ebfbd2-b113-de0e-04ca-62149e5ae34d-e0473f29"]}
+          className={style.hero_photo}
+        >
+          {/* <div className={style.video}>
+            <div className={`${style.video_section} ${style.background_video}`}>
+              <video
+                autoPlay
+                loop
+                muted
+              >
+                <source
+                  src="../assets/Aquiaklin_video.mp4" type="video/mp4"
+                />
+                <source
+                />
+              </video>
+
+            </div>
+
+          </div> */}
+          <Link
+            href="/"
+            className={`${style.lightbox_link} ${style.w_inline_block} ${style.w_lightbox}`}
           >
-            By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src={effect_1}
+              loading="lazy"
+              width="318.5"
+              sizes="102px"
+              alt=""
+              className={style.play_showreel}
             />
-          </a>
+            <div className={style.play_content}>
+
+              <div
+                target="_blank"
+                className={`${style.circle_link_block} ${style.w_inline_block}`}
+                href="https://www.instagram.com/aquaklin.ve/"
+              >
+                <img
+                  alt=""
+                  loading="lazy"
+                  width={60}
+                  height={60}
+                  className={style.social_media_icon}
+                  src="https://galeryan.com/_next/static/media/instagram.6592c30f.svg"
+                />
+              </div>
+
+              {/* <div
+                className={style.play_text}
+                style={{
+                  transform:
+                    "translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+                  transformStyle: "preserve-3d"
+                }}
+              >
+                Play
+              </div>
+              <div
+                className={style.play_text}
+                style={{
+                  transform:
+                    "translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+                  transformStyle: "preserve-3d"
+                }}
+              >
+                Play
+              </div> */}
+            </div>
+          </Link>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+
   )
 }
